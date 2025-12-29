9b174cbdb99d5faf866aef0b525c13146e71e08a
Anil Erman <egeanil0596@gmail.com>
2025-12-29 15:06:05 +0200

    son gğncellemeler yapıldı fotolar kücültüldü ve github deployment eklendi

--- Halil/scripts/home-slider.js
+++ Halil/scripts/home-slider.js
@@ -1,5 +1,5 @@
 let index = 0;
-const images = ['home1.jpg', 'home2.jpg', 'home3.jpg', 'home4.jpg', 'home5.jpg'];
+const images = ['home1.webp', 'home2.webp', 'home3.webp', 'home4.webp', 'home5.webp'];
 setInterval(() => {
   document.querySelector('.hero').style.backgroundImage = `url('assets/home/${images[index]}')`;
   index = (index + 1) % images.length;
--- Halil/tet_architecture_html_full/scripts/home-slider.js
+++
@@ -1,6 +0,0 @@
-let index = 0;
-const images = ['home1.jpg', 'home2.jpg', 'home3.jpg', 'home4.jpg', 'home5.jpg'];
-setInterval(() => {
-  document.querySelector('.hero').style.backgroundImage = `url('assets/home/${images[index]}')`;
-  index = (index + 1) % images.length;
-}, 10000);
