2d332497e083c2145e83eaa1f7e45a4ad8dc84fd
Anil Erman <egeanil0596@gmail.com>
2025-12-30 21:08:27 +0200

    PHP mail kodu yazıldı ve test edilmek için gönderildi

---
+++ Halil/send_email.php
@@ -0,0 +1,41 @@
+<?php
+
+    $to = "tet@tetarchitects.com";
+    $from = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+    $name = htmlspecialchars($_REQUEST['name']);
+    $subject = htmlspecialchars($_REQUEST['subject']);
+    $cmessage = htmlspecialchars($_REQUEST['message']);
+
+    $headers = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: " . $from . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    $subject = "You have a message from your TET Architects website.";
+
+    $logo = 'img/logo.png';
+    $link = '#';
+
+    // Mail içeriği
+    $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+    $body .= "<table style='width: 100%;'>";
+    $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+    $body .= "<a href='{$link}'><img src='{$logo}' alt='Logo'></a><br><br>";
+    $body .= "</td></tr></thead><tbody><tr>";
+    $body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+    $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+    $body .= "</tr>";
+    $body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+    $body .= "<tr><td></td></tr>";
+    $body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+    $body .= "</tbody></table>";
+    $body .= "</body></html>";
+
+    // Mail gönderme
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Mesajınız başarıyla gönderildi.";
+    } else {
+        echo "Mesaj gönderilirken bir hata oluştu.";
+    }
+
+?>
